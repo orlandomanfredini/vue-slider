23188ac8ed085bfd0b347c07de3d53af64acff9b
orlando manfredini <orlando.manfre1@gmail.com>
2024-03-26 16:41:35 +0100

    ADD: commit

--- js/app.js
+++ js/app.js
@@ -3,6 +3,8 @@ const { createApp } = Vue
   createApp({
     data() {
       return {
+        activeImg : 0,
+        lastImg : 4,
         message : 'Slider Vue',
         slides : [
                 {
@@ -10,23 +12,41 @@ const { createApp } = Vue
                     title: 'Marvel\'s Spiderman Miles Morale',
                     text: 'Experience the rise of Miles Morales as the new hero masters incredible, explosive new powers to become his own Spider-Man.',
                 }, {
-                    image: 'img/02.webp',
+                    image: './img/02.webp',
                     title: 'Ratchet & Clank: Rift Apart',
                     text: 'Go dimension-hopping with Ratchet and Clank as they take on an evil emperor from another reality.',
                 }, {
-                    image: 'img/03.webp',
+                    image: './img/03.webp',
                     title: 'Fortnite',
                     text: "Grab all of your friends and drop into Epic Games Fortnite, a massive 100 - player face - off that combines looting, crafting, shootouts and chaos.",
                 }, {
-                    image: 'img/04.webp',
+                    image: './img/04.webp',
                     title: 'Stray',
                     text: 'Lost, injured and alone, a stray cat must untangle an ancient mystery to escape a long-forgotten city',
                 }, {
-                    image: 'img/05.webp',
+                    image: './img/05.webp',
                     title: "Marvel's Avengers",
                     text: 'Marvel\'s Avengers is an epic, third-person, action-adventure game that combines an original, cinematic story with single-player and co-operative gameplay.',
                 }
             ],
         }
+    },
+    methods: {
+        prev(){
+            this.activeImg--
+        },
+
+        next(){
+            this.activeImg++
+        },
+
+        returnLastImg(){
+            if(this.activeImg===this.lastImg){
+                this.activeImg = 0;
+                return activeImg
+            }
+        }
+
+        
     }
   }).mount('#app')
